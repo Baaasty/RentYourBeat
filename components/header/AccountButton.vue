0c758924f6ee638cf7d5fbadbfe1cd22b6f3912f
Baaasty <46427883+Baaasty@users.noreply.github.com>
2024-05-26 10:59:15 +0200

    Fix typo

--- components/header/AccountButton.vue
+++ components/header/AccountButton.vue
@@ -1,7 +1,7 @@
 <template>
   <NuxtLink
     v-if="user"
-    :="{ name: 'dashboard' }"
+    :to="{ name: 'dashboard' }"
     class="rounded-full bg-blue-500 px-4 py-2 font-semibold text-white duration-150 ease-in-out hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-300 focus:ring-opacity-50"
   >
     Dein Dashboard
