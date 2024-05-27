4c36f6ded7345e800a3346f680d432c58c62f280
Baaasty <46427883+Baaasty@users.noreply.github.com>
2024-05-27 23:33:52 +0200

    seo opti

--- app.vue
+++ app.vue
@@ -5,20 +5,12 @@
 </template>
 
 <script setup lang="ts">
-useHead({
-  meta: [
-    {
-      name: "theme-color",
-      content: "#f69013",
-    },
-  ],
-});
-
 useSeoMeta({
   ogTitle: "RentYourBeat - Miete Eventtechnik ganz einfach online.",
   description:
     "Miete Eventtechnik für deine Feier einfach online. Keine Anrufe, keine E-Mails - alles über unser Dashboard. Also, worauf wartest du? Lass die Party steigen!",
   ogDescription: "Miete Eventtechnik für deine Feier ganz einfach online.",
+  themeColor: "#f69013",
 });
 </script>
 
